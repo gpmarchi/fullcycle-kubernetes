729a345055a0d7fa21989b68ad158100567e3637
gpmarchi <gustavomarchi@gmail.com>
2022-07-27 17:26:39 -0300

    feat: add simple http server application

---
+++ server.js
@@ -0,0 +1,15 @@
+import http from 'http';
+
+const host = '0.0.0.0';
+const port = 3333;
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("My first server!!!");
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
