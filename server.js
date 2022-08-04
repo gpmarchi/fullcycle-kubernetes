31c119b896cb6176b5404a9cf9bb88eba7cf4dcf
gpmarchi <gustavomarchi@gmail.com>
2022-08-04 11:41:38 -0300

    feat: add dispatcher and new route for reading file

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 import http from 'http';
+import fs from 'fs/promises';
+import HttpDispatcher from 'httpdispatcher';
 
 const host = '0.0.0.0';
 const port = 3333;
 
-const requestListener = function (req, res) {
+const dispatcher = new HttpDispatcher();
+
+dispatcher.onGet("/", function(request, response) {
 	const name = process.env.NAME;
 	const age = process.env.AGE;
 
-  res.writeHead(200);
-  res.end(`Hello, I'm ${name}. I'm ${age} years old.`);
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end(`<h1>Hello, I'm ${name}. I'm ${age} years old.</h1>`);
+});
+
+dispatcher.onGet("/configmap", async function (request, response) {
+  try {
+    const data = await fs.readFile('./myfamily/family.txt', { encoding: 'utf8' });
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+dispatcher.onError(function(request, response) {
+  response.writeHead(404);
+  response.end("Error, the URL doesn't exist");
+});
+
+const requestListener = function (request, response) {
+	try {
+		console.log(request.url);
+		dispatcher.dispatch(request, response);
+	} catch (error) {
+    console.error(error);
+	}
 };
 
 const server = http.createServer(requestListener);
